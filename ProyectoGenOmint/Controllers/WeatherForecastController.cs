9691a10867780b17efc769a41bd1956a1878324f
francobernal03 <francober03@gmail.com>
2026-01-22 17:36:37 -0300

    Cambios tecnicos
    se ha movido userRandom a services, models solo admite entidades, creaccion de carpeta para mover archivos que no se van a utilizar (en caso de que se quiera recuperar archivo antiguo)

--- ProyectoGenOmint/Controllers/WeatherForecastController.cs
+++ ProyectoGenOmint/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+//implementacion del patron de inyeccion de dependencia
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProyectoGenOmint.Controllers
@@ -21,6 +23,7 @@ namespace ProyectoGenOmint.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //cambios en el array para que acepte el patron nuevo
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
